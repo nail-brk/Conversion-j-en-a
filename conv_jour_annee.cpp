5ce330ebb7f5472e7675468a82cb575925d8f1b6
nail-brk <briknail@gmail.com>
2025-07-28 22:15:09 +0200

    Affichage graphique

--- conv_jour_annee.cpp
+++ conv_jour_annee.cpp
@@ -1,20 +1,3 @@
-#include <iostream>
-using namespace std;
 
-int main ()
-{
-    int nombre, sem, jour, mois, annee, reste;
-    do
-    {
-        cout<<"donnez un entier"<<endl;
-        cin >> nombre ;
-    } while (nombre<0);
-    reste= nombre % 365;
-    mois = reste / 30;
-    reste=reste % 30;
-    annee = nombre / 365;
-    sem = reste / 7;
-    jour = reste % 7;
     cout<<nombre<<" jours = "<<annee<<" année "<<mois<<" mois "<<sem<<" semaines et "<<jour<<" jours"<<endl;
-    return 0;
-}
+ 
